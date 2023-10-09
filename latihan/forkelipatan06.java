579575dc05a9484793e6e63ab3b8db84ee0a67de
AtabikM1 <mutawakilatabik@gmail.com>
2023-10-09 15:04:16 +0700

    forkelipatan

--- latihan/forkelipatan06.java
+++ latihan/forkelipatan06.java
@@ -2,14 +2,15 @@ package latihan;
 import java.util.Scanner;
 
 public class forkelipatan06 {
-
+    //buat program main
     public static void main(String [] args){
-        Scanner scan = new Scanner(System.in);
-        int jumlah = 0, kelipatan, counter = 0;
+        Scanner scan = new Scanner(System.in); //buat library scanner 
+        int jumlah = 0, kelipatan, counter = 0; //inisialisasi variabel
         
 
         System.out.println("Masukkan bilangan kelipatan (1-9) : ");
-        kelipatan = scan.nextInt();
+        kelipatan = scan.nextInt(); // menambah code untuk menerima dari input dari keyboard
+            //Buat struktur perulangan FOR dengan kondisi pemilihan IF 
 
         for (int i = 1; i <= 50; i++){
             if (i % kelipatan == 0){
@@ -17,6 +18,8 @@ public class forkelipatan06 {
                 counter++;
             }
         }
+        //Buat struktur perulangan FOR dengan kondisi pemilihan IF 
+
         System.out.printf("banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
     }
