e0e1bec09afc83cd1b23c69d16a5b176d7bdc2b8
AtabikM1 <mutawakilatabik@gmail.com>
2023-11-06 13:55:19 +0700

    for nested

---
+++ Pertemuan11/square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class square {
+
+    public static void main(String[] args) { // membuat  method main(),
+        Scanner sc = new Scanner(System.in); // import class Scanner.
+
+        System.out.print("masukkan nilai N = ");
+        int N = sc.nextInt(); 
+
+        for(int j = 1; j <= N; j++){ // outer untuk loop kebawah
+            for (int i = 0 ; i <= N ; i++){ // inner loop yang mencetak bintang
+                System.out.print(" * ");
+            }
+            
+        }
+        
+    }
+}
